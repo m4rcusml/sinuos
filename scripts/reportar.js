5ea8d494617a4dabe8841d927ae4f8f63a592c4e
Marcus com U <114195461+m4rcusml@users.noreply.github.com>
2023-06-01 22:34:41 -0400

    algumas correções

--- scripts/reportar.js
+++ scripts/reportar.js
@@ -1,5 +1,10 @@
 const btnReportarErro = document.querySelector('#btn-reportar-erro')
 btnReportarErro.addEventListener('click', () => {
     const erro = prompt('Dê detalhes sobre o que está acontecendo:')
-    alert(`O erro reportado foi:\n"${erro}"`)
+    if(erro) {
+        const enderecoEmail = prompt('Informe o seu endereço de email:')
+        if(enderecoEmail) {
+            setTimeout(() => alert('O erro foi reportado com sucesso!'), 800)
+        }
+    }
 })
