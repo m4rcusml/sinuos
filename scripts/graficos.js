a3860945c1193e35c21e48dd62f6e1e6159665de
Marcus com U <114195461+m4rcusml@users.noreply.github.com>
2023-06-07 21:52:46 -0400

    pagina gráficos finalizada
    Finalizei a nova página de gráficos e corrigi alguns erros

--- scripts/graficos.js
+++ scripts/graficos.js
@@ -1,26 +1,65 @@
-const sitesGraficos = ['ph', 'temperatura', 'alcalinidade']
-const iframe = document.querySelector('#graficos-container iframe')
-const btnAnterior = document.querySelector('#btn-left')
-const btnProximo = document.querySelector('#btn-right')
-let atual = 0
+google.charts.load('current', { 'packages': ['corechart'] })
+google.charts.setOnLoadCallback(drawChart)
 
-btnAnterior.addEventListener('click', () => {
-    if (atual === 0) {
-        atual = sitesGraficos.length - 1;
-    } else {
-        atual--;
-    }
-    atualizarIframe();
-})
-btnProximo.addEventListener('click', () => {
-    if (atual === sitesGraficos.length - 1) {
-        atual = 0;
-    } else {
-        atual++;
-    }
-    atualizarIframe();
-})
+function drawChart() {
+    graficoAlcalinidade()
+    graficoPh()
+    graficoTemperatura()
+}
 
-function atualizarIframe() {
-    iframe.src = `graficos/${sitesGraficos[atual]}.html`;
+function graficoAlcalinidade() {
+    const data = google.visualization.arrayToDataTable([
+        ['Dia', 'Alcalinidade'],
+        ['1', 15],
+        ['2', 7],
+        ['3', 9],
+        ['4', 12]
+    ])
+
+    const options = {
+        title: 'Alcalinidade nas últimas 4 semanas',
+        hAxis: { title: 'Semanas', titleTextStyle: { color: '#00ff00' } },
+        vAxis: { minValue: 0 }
+    }
+
+    const chart = new google.visualization.AreaChart(document.getElementById('grafico-alcalinidade'))
+    chart.draw(data, options)
+}
+
+function graficoPh() {
+    const data = google.visualization.arrayToDataTable([
+        ['Dia', 'PH'],
+        ['1', 6],
+        ['2', 7.2],
+        ['3', 5.3],
+        ['4', 6.7]
+    ]);
+
+    const options = {
+        title: 'PH nas últimas 4 semanas',
+        hAxis: { title: 'Semanas', titleTextStyle: { color: '#0000ff' } },
+        vAxis: { minValue: 0 }
+    };
+
+    const chart = new google.visualization.AreaChart(document.getElementById('grafico-ph'));
+    chart.draw(data, options);
+}
+
+function graficoTemperatura() {
+    const data = google.visualization.arrayToDataTable([
+    ['Dia', 'Temperatura'],
+        ['1', 27],
+        ['2', 24],
+        ['3', 29],
+        ['4', 22]
+    ]);
+
+    const options = {
+    title: 'Temperatura nas últimas 4 semanas',
+        hAxis: { title: 'Semanas', titleTextStyle: { color: '#f00' } },
+        vAxis: { minValue: 0 }
+    };
+
+    const chart = new google.visualization.AreaChart(document.getElementById('grafico-temperatura'));
+    chart.draw(data, options);
 }
